01b933143daa6ed05a4f585a7085cc9df37ec3f6
Ruben Villar <ruben@rubenvillar.net>
2019-02-02 14:27:03 +0100

    first iteration

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,56 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/User.js");
+const router = express.Router();
+
+const saltRounds = 1;
+const salt = bcrypt.genSaltSync(saltRounds);
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.post("/", (req, res, next) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  User.findOne({ userName: userName })
+    .then(user => {
+      if (!user) {
+        res.render("index", {
+          errorMessage: "The username doesn`t exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("index", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res, next) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  const hash = bcrypt.hashSync(password, salt);
+  console.log(userName, hash);
+  User.create({ userName: userName, password: hash })
+    .then(user => {
+      console.log(user);
+      res.redirect("/");
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
